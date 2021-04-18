ec4ad58d5d305846c619460c0e39e6932daa59f0
francescobianco <info.francescobianco@gmail.com>
2021-04-18 12:37:21 +0200

    first commit

--- sharp.sh
+++ sharp.sh
@@ -6,5 +6,10 @@ while IFS= read line || [[ -n "${line}" ]]; do
     line=$(echo -e "${line}" | sed 's/[^\b]\b//')
     test=$(echo -e "${line}" | tr -d '\b')
   done && true
-  echo -e "${line}" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g'
+  feed=
+  IFS=$'\r'
+  for part in $line; do
+    [[ ${#part} < ${#feed} ]] && feed="${part}${feed:${#part}}" || feed="${part}"
+  done && true
+  echo -e "${feed}" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g'
 done && true
--- test/output-test.sh
+++ test/output-test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-printf "1234\b\b\b\b4321" | ./sharp.sh
+## Test case
+#printf "1234\b\b\b\b4321" | ./sharp.sh
+#printf "12345\r4321" | ./sharp.sh
 
+## examples
+#cat v1 > a1
+#cat v1 | ./sharp.sh > a2
+#curl -o v https://filesamples.com/samples/video/mp4/sample_3840x2160.mp4 > v1 2>&1
+#curl -o v https://filesamples.com/samples/video/mp4/sample_3840x2160.mp4 | ./sharp.sh > v2 2>&1
 
